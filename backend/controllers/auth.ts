119da89790da0c63ca619db44bb5cf5c81abaa41
Prajwal P L <prajwalpl096@gmail.com>
2026-05-02 17:43:16 +0530

    feat(auth): enhance user registration and login with JWT token generation

--- backend/controllers/auth.ts
+++ backend/controllers/auth.ts
@@ -3,6 +3,7 @@ import bcrypt from 'bcrypt';
 import { db } from '../lib/utils';
 import { users } from '../db/schema';
 import { eq } from 'drizzle-orm';
+import jwt from 'jsonwebtoken';
 
 export const registerHandler: RequestHandler = async (req, res) => {
     const { fullName, email, password } = req.body
@@ -11,22 +12,36 @@ export const registerHandler: RequestHandler = async (req, res) => {
             return res.status(400).send("Missing required fields")
         }
 
-        const existingUser = await db.select().from(users).where(eq(users.email, email));
-        if (existingUser) {
+        const existingUser = await db.select().from(users).where(eq(users.email, email)).limit(1);
+        if (existingUser.length > 0) {
             return res.status(400).send("User with this email already exists")
         }
 
         const hashedPassword = await bcrypt.hash(password, 10)
 
-        const newUser = await db.insert(users).values({
-            fullName,
-            email,
-            password: hashedPassword,
-        })
+        const [newUser] = await db
+            .insert(users)
+            .values({
+                fullName,
+                email,
+                password: hashedPassword,
+            })
+            .returning({
+                id: users.id,
+                email: users.email,
+                fullName: users.fullName,
+                provider: users.provider,
+                role: users.role,
+                createdAt: users.createdAt,
+            })
 
-        console.log(newUser)
+        console.log("New user created with ID: ", newUser)
 
-        res.status(201).send("User registered successfully")
+        const token = jwt.sign({
+            userId: newUser?.id,
+        }, process.env.JWT_SECRET_KEY as string, { expiresIn: '30d' })
+
+        res.status(201).json({ message: "User registered successfully", user: newUser, token })
 
     } catch (error) {
         console.log("Register endpoint failed with error: ", error)
@@ -41,9 +56,7 @@ export const loginHandler: RequestHandler = async (req, res) => {
             return res.status(400).send("Missing required fields")
         }
 
-        const result = await db.select().from(users).where(eq(users.email, email)).limit(1)
-
-        const user = result.flat()[0]
+        const [user] = await db.select().from(users).where(eq(users.email, email)).limit(1)
 
         if (!user) {
             return res.status(400).send("Invalid email or password")
@@ -55,7 +68,9 @@ export const loginHandler: RequestHandler = async (req, res) => {
             return res.status(400).send("Invalid email or password")
         }
 
-        res.status(200).send("Login successful")
+        const token = jwt.sign({ userId: user.id, }, process.env.JWT_SECRET_KEY as string, { expiresIn: '30d' })
+
+        res.status(200).json({ message: "Login successful", user: user, token })
 
     } catch (error) {
         console.log("Login endpoint failed with error: ", error)
@@ -71,9 +86,7 @@ export const profileHandler: RequestHandler = async (req, res) => {
             return res.status(401).send("Unauthorized")
         }
 
-        const result = await db.select().from(users).where(eq(users.id, userId)).limit(1)
-
-        const user = result.flat()[0]
+        const [user] = await db.select().from(users).where(eq(users.id, userId)).limit(1)
 
         if (!user) {
             return res.status(404).send("User not found")
